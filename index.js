4836e9f0e96f7849ba29d8d1c2518ed27c380164
Nobert-Muma <mumanobert17@gmail.com>
2024-07-24 21:02:52 +0300

    Implemented hasTargetSum function to iterate through an array of integers and check if any pair of numbers sums up to a specified target integer. This function uses nested loops to compare each pair of elements and returns true if a valid pair is found.

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+    // n iterations
+  for(let i=0; i<array.length; i++){
+      // n iterations
+    for(let j=i+1; j<array.length; j++){
+        // 1 step
+      let sum=array[i]+array[j];
+        // 1 step
+      if(sum===target){
+        return true;
+      }
+    }
+  }
+  return false;
 }
-
+console.log("=>true",hasTargetSum([3, 4, 5, 7, 9], 11));
+console.log("=>true",hasTargetSum([3, 4, 5, 7, 9], 12));
+console.log("=>true",hasTargetSum([3, 4, 5, 7, 9], 14));
+console.log("=>true",hasTargetSum([3, 4, 5, 7, 9], 10));
+console.log("=>false",hasTargetSum([3, 4, 5, 7, 9], 2));
+console.log("=>false",hasTargetSum([3, 4, 5, 7, 9], 6));
+console.log("=>false",hasTargetSum([3, 4, 5, 7, 9], 17));
+console.log("=>false",hasTargetSum([3, 4, 5, 7, 9], 18));
+console.log("=>false",hasTargetSum([3, 4, 5, 7, 9], 19));
 /* 
   Write the Big O time complexity of your function here
+    n*n+1+1
+    O(n^2+2)
+    O(n^2)
+    quadratic time
 */
 
 /* 
